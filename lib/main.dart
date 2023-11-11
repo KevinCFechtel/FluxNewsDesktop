46f52ee28e703360352f20c7096006e22cefd9b1
Kevin Fechtel <kevin.fechtel@circle-dev.de>
2023-11-11 14:25:04 +0100

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_acrylic/flutter_acrylic.dart' as flutter_acrylic;
+import 'package:flux_news_desktop/database_backend.dart';
+import 'package:flux_news_desktop/flux_news_counter_state.dart';
 import 'package:flux_news_desktop/flux_news_state.dart';
+import 'package:flux_news_desktop/search.dart';
+import 'package:flux_news_desktop/sync_news.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:system_theme/system_theme.dart';
 import 'package:window_manager/window_manager.dart';
@@ -58,54 +63,184 @@ class FluxNewsDesktop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => FluxNewsState(),
-      builder: (context, child) {
-        return FluentApp(
-          title: FluxNewsState.applicationName,
-          themeMode: ThemeMode.system,
-          debugShowCheckedModeBanner: false,
-          color: Colors.blue,
-          darkTheme: FluentThemeData(
-            brightness: Brightness.dark,
-            accentColor: Colors.blue,
-            visualDensity: VisualDensity.standard,
-            focusTheme: FocusThemeData(
-              glowFactor: is10footScreen(context) ? 2.0 : 0.0,
-            ),
-          ),
-          theme: FluentThemeData(
-            accentColor: Colors.blue,
-            visualDensity: VisualDensity.standard,
-            focusTheme: FocusThemeData(
-              glowFactor: is10footScreen(context) ? 2.0 : 0.0,
-            ),
-          ),
-          localizationsDelegates: AppLocalizations.localizationsDelegates,
-          supportedLocales: const [
-            Locale('en', ''),
-            Locale('de', ''),
-          ],
-          home: NavigationView(
-            appBar: const NavigationAppBar(
-              title: Text(FluxNewsState.applicationName),
-            ),
-            pane: NavigationPane(displayMode: PaneDisplayMode.auto, items: [
-              PaneItem(
-                  icon: const Icon(FluentIcons.home),
-                  title: const Text("Home"),
-                  body: const SizedBox.shrink()),
-              PaneItem(
-                  icon: const Icon(FluentIcons.insert),
-                  title: const Text("Insert"),
-                  body: const SizedBox.shrink()),
-              PaneItem(
-                  icon: const Icon(FluentIcons.view),
-                  title: const Text("View"),
-                  body: const SizedBox.shrink())
-            ]),
-          ),
-        );
-      },
+        create: (context) => FluxNewsState(),
+        builder: (context, child) {
+          return ChangeNotifierProvider(
+            create: (context) => FluxNewsCounterState(),
+            builder: (context, child) {
+              return FluentApp(
+                title: FluxNewsState.applicationName,
+                themeMode: ThemeMode.system,
+                debugShowCheckedModeBanner: false,
+                color: Colors.blue,
+                darkTheme: FluentThemeData(
+                  brightness: Brightness.dark,
+                  accentColor: Colors.blue,
+                  visualDensity: VisualDensity.standard,
+                  focusTheme: FocusThemeData(
+                    glowFactor: is10footScreen(context) ? 2.0 : 0.0,
+                  ),
+                ),
+                theme: FluentThemeData(
+                  accentColor: Colors.blue,
+                  visualDensity: VisualDensity.standard,
+                  focusTheme: FocusThemeData(
+                    glowFactor: is10footScreen(context) ? 2.0 : 0.0,
+                  ),
+                ),
+                localizationsDelegates: AppLocalizations.localizationsDelegates,
+                supportedLocales: const [
+                  Locale('en', ''),
+                  Locale('de', ''),
+                ],
+                // define routes for main view (FluxNewsBody), settings view and search view
+                routes: {
+                  //FluxNewsState.rootRouteString: (context) =>
+                  //    const FluxNewsBody(),
+                  //FluxNewsState.settingsRouteString: (context) =>
+                  //    const Settings(),
+                  FluxNewsState.searchRouteString: (context) => const Search(),
+                },
+                home: const MainViewWidget(),
+              );
+            },
+          );
+        });
+  }
+}
+
+class MainViewWidget extends StatelessWidget {
+  const MainViewWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationView(
+      appBar: const NavigationAppBar(
+        leading: Icon(FontAwesomeIcons.bookOpen),
+        title: Text(FluxNewsState.applicationName),
+        actions: AppBarButtons(),
+      ),
+      pane: NavigationPane(displayMode: PaneDisplayMode.auto, items: [
+        PaneItem(
+            icon: const Icon(FluentIcons.boards),
+            title: const Text("Home"),
+            body: const SizedBox.shrink()),
+        PaneItem(
+          icon: const Icon(FluentIcons.search),
+          title: Text(AppLocalizations.of(context)!.search),
+          body: const SizedBox.shrink(),
+          onTap: () {
+            Navigator.pushNamed(context, FluxNewsState.searchRouteString);
+          },
+        ),
+        PaneItem(
+            icon: const Icon(FluentIcons.settings),
+            title: Text(AppLocalizations.of(context)!.settings),
+            body: const SizedBox.shrink())
+      ]),
+      /*
+      content: Center(
+          child: Text(
+        AppLocalizations.of(context)!.noNewEntries,
+      )),
+      */
+    );
+  }
+}
+
+class AppBarButtons extends StatelessWidget {
+  const AppBarButtons({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    FluxNewsState appState = context.watch<FluxNewsState>();
+    FluxNewsCounterState appCounterState =
+        context.watch<FluxNewsCounterState>();
+    return Row(
+      children: [
+        IconButton(
+          onPressed: () async {
+            await syncNews(appState, context);
+          },
+          icon: appState.syncProcess
+              ? const SizedBox(
+                  height: 15.0,
+                  width: 15.0,
+                  //child: //CircularProgressIndicator.adaptive(),
+                )
+              : const Icon(
+                  FluentIcons.refresh,
+                ),
+        ),
+        IconButton(
+          onPressed: () async {
+            // switch between newest first and oldest first
+            // if the current sort order is newest first change to oldest first
+            if (appState.sortOrder ==
+                FluxNewsState.sortOrderNewestFirstString) {
+              // switch the state to all news
+              appState.sortOrder = FluxNewsState.sortOrderOldestFirstString;
+
+              // save the state persistent
+              appState.storage.write(
+                  key: FluxNewsState.secureStorageSortOrderKey,
+                  value: FluxNewsState.sortOrderOldestFirstString);
+
+              // refresh news list with the all news state
+              appState.newsList =
+                  queryNewsFromDB(appState, appState.feedIDs).whenComplete(() {
+                waitUntilNewsListBuild(appState).whenComplete(
+                  () {
+                    context
+                        .read<FluxNewsState>()
+                        .itemScrollController
+                        .jumpTo(index: 0);
+                  },
+                );
+              });
+
+              // notify the categories to update the news count
+              appCounterState.listUpdated = true;
+              appCounterState.refreshView();
+              appState.refreshView();
+              // if the current sort order is oldest first change to newest first
+            } else {
+              // switch the state to show only unread news
+              appState.sortOrder = FluxNewsState.sortOrderNewestFirstString;
+
+              // save the state persistent
+              appState.storage.write(
+                  key: FluxNewsState.secureStorageSortOrderKey,
+                  value: FluxNewsState.sortOrderNewestFirstString);
+
+              // refresh news list with the only unread news state
+              appState.newsList =
+                  queryNewsFromDB(appState, appState.feedIDs).whenComplete(() {
+                waitUntilNewsListBuild(appState).whenComplete(
+                  () {
+                    context
+                        .read<FluxNewsState>()
+                        .itemScrollController
+                        .jumpTo(index: 0);
+                  },
+                );
+              });
+
+              // notify the categories to update the news count
+              appCounterState.listUpdated = true;
+              appCounterState.refreshView();
+              appState.refreshView();
+            }
+          },
+          icon: appState.newsStatus == FluxNewsState.unreadNewsStatus
+              ? const Icon(FluentIcons.read)
+              : const Icon(FluentIcons.accept),
+        )
+      ],
     );
   }
 }
---
+++ lib/search.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:flux_news_desktop/logging.dart';
+import 'package:flux_news_desktop/search_news_list.dart';
+import 'package:logger/logger.dart';
+import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_gen/gen_l10n/flux_news_localizations.dart';
+
+import 'flux_news_state.dart';
+import 'miniflux_backend.dart';
+import 'news_model.dart';
+
+class Search extends StatelessWidget {
+  const Search({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    FluxNewsState appState = context.watch<FluxNewsState>();
+
+    return FluxNewsSearchStatefulWrapper(onInit: () {
+      initConfig(context, appState);
+    }, child: OrientationBuilder(builder: (context, orientation) {
+      appState.orientation = orientation;
+      return searchLayout(context, appState);
+    }));
+  }
+
+  // initConfig reads the config values from the persistent storage and sets the state
+  // accordingly.
+  // It also initializes the database connection.
+  Future<void> initConfig(BuildContext context, FluxNewsState appState) async {
+    await appState.readConfigValues();
+    if (context.mounted) {
+      appState.readConfig(context);
+    }
+  }
+
+  Scaffold searchLayout(BuildContext context, FluxNewsState appState) {
+    return Scaffold(
+        appBar: AppBar(
+          // set the title of the search page to search text field
+          title: TextField(
+            controller: appState.searchController,
+            style: Theme.of(context).textTheme.bodyLarge,
+            decoration: InputDecoration(
+              hintText: AppLocalizations.of(context)!.searchHint,
+              hintStyle: Theme.of(context).textTheme.bodyLarge,
+              border:
+                  UnderlineInputBorder(borderRadius: BorderRadius.circular(2)),
+              suffixIcon: IconButton(
+                onPressed: () {
+                  appState.searchController.clear();
+                  appState.searchNewsList = Future<List<News>>.value([]);
+                  appState.refreshView();
+                },
+                icon: const Icon(Icons.clear),
+              ),
+            ),
+
+            // on change of the search text field, fetch the news list
+            onSubmitted: (value) async {
+              if (value != '') {
+                // fetch the news list from the backend with the search text
+                Future<List<News>> searchNewsListResult =
+                    fetchSearchedNews(http.Client(), appState, value)
+                        .onError((error, stackTrace) {
+                  logThis(
+                      'fetchSearchedNews',
+                      'Caught an error in fetchSearchedNews function! : ${error.toString()}',
+                      Level.error);
+                  if (appState.errorString !=
+                      AppLocalizations.of(context)!
+                          .communicateionMinifluxError) {
+                    appState.errorString = AppLocalizations.of(context)!
+                        .communicateionMinifluxError;
+                    appState.newError = true;
+                    appState.refreshView();
+                  }
+                  return [];
+                });
+                // set the state with the fetched news list
+                appState.searchNewsList = searchNewsListResult;
+                appState.refreshView();
+              } else {
+                // if search text is empty, set the state with an empty list
+                appState.searchNewsList = Future<List<News>>.value([]);
+                appState.refreshView();
+              }
+            },
+          ),
+        ),
+        // show the news list
+        body: const SearchNewsList());
+  }
+}
+
+class FluxNewsSearchStatefulWrapper extends StatefulWidget {
+  final Function onInit;
+  final Widget child;
+  const FluxNewsSearchStatefulWrapper(
+      {super.key, required this.onInit, required this.child});
+  @override
+  FluxNewsBodyState createState() => FluxNewsBodyState();
+}
+
+// extend class to save actual scroll state of the list view
+class FluxNewsBodyState extends State<FluxNewsSearchStatefulWrapper> {
+  // init the state of FluxNewsBody to load the config and the data on startup
+  @override
+  void initState() {
+    widget.onInit();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+}
